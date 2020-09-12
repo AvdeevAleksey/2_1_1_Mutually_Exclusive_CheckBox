7b3502c807b02b77bc529fb1e536b65f14beb416
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-12 11:54:03 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a2_1_1_mutually_exclusive_checkbox/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.avdeev.android.a2_1_1_mutually_exclusive_checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
