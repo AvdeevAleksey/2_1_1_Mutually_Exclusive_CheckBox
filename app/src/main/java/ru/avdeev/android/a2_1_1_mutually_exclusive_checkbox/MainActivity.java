f5a96d39dd648e36a928c74e65f21c7903e0e900
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-12 13:05:33 +0300

    The added items are in MainActivity, initiated by the fields of objects.

--- app/src/main/java/ru/avdeev/android/a2_1_1_mutually_exclusive_checkbox/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a2_1_1_mutually_exclusive_checkbox/MainActivity.java
@@ -3,12 +3,25 @@ package ru.avdeev.android.a2_1_1_mutually_exclusive_checkbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextAmount;
+    EditText editTextDescription;
+    CheckBox checkBoxBank;
+    CheckBox checkBoxMobile;
+    CheckBox checkBoxCash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextAmount = findViewById(R.id.amountEditText);
+        editTextDescription = findViewById(R.id.descriptionEditText);
+        checkBoxBank = findViewById(R.id.bankCardCheckBox);
+        checkBoxMobile = findViewById(R.id.mobileCheckBox);
+        checkBoxCash = findViewById(R.id.cashCheckBox);
     }
 }
